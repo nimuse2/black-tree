92ff1bd8bf7cbf22b824315e9544b7b7d17bf1ad
Nick Walters <nick.walters@argandsolutions.com>
2020-08-26 20:51:20 +0100

    Updates

---
+++ precache-manifest.c588bd30bba4100457aeb826a0bc0f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab6cc12a43e980d1d725e2bf3a4fb43",
+    "url": "/black-tree/index.html"
+  },
+  {
+    "revision": "6df56eb3ec141a55eefe",
+    "url": "/black-tree/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "48549833c86c3217ad9e",
+    "url": "/black-tree/static/css/main.8db1cf44.chunk.css"
+  },
+  {
+    "revision": "6df56eb3ec141a55eefe",
+    "url": "/black-tree/static/js/2.761649b9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/black-tree/static/js/2.761649b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48549833c86c3217ad9e",
+    "url": "/black-tree/static/js/main.7ba6a33c.chunk.js"
+  },
+  {
+    "revision": "7acb07210be6ff7aeba2",
+    "url": "/black-tree/static/js/runtime-main.b8d61019.js"
+  }
+]);
